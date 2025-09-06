9704c7d15368467e44bce3228d754a003a33353a
fredsrocha <22266020+FredSRocha@users.noreply.github.com>
2025-09-06 16:19:01 -0300

    first commit

---
+++ src/main/java/br/com/fredsrocha/deesafio_todolist/DeesafioTodolistApplication.java
@@ -0,0 +1,13 @@
+package br.com.fredsrocha.deesafio_todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeesafioTodolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeesafioTodolistApplication.class, args);
+	}
+
+}
